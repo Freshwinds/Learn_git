fa335e6b69d409629b54d3d5cc9fcf0bbfe0e850
Freshwinds <m13720362902@163.com>
2017-06-21 15:03:40 +0800

    a new js

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 module.exports = function main() {
-    var x= window.prompt("请输入第一个数");
-	var y= window.prompt("请输入第二个数")
-    return x%y;
+    var x= window.prompt("请输入第一个数","")
+	var y= window.prompt("请输入第二个数","")
+    return x%y
 };
