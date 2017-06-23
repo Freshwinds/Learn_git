595535ac6f96926ac8395a5d4c219163ea36e2d3
Freshwinds <m13720362902@163.com>
2017-06-24 00:20:18 +0800

    a new file

--- main/main.js
+++ main/main.js
@@ -1,26 +1,3 @@
-module.exports = function main() {
-    class Person{
-		constructor(name,age){
-			this.name=name;
-			this.age=age;
-		}
-		introduce(){
-			return 'My name is ' +this.name+'.'+' I am '+this.age+' years old.';
-		}
-	}
-	class Student extends Person{
-		constructor(name,age,klass){
-			super(name,age);
-			this.klass=klass;
-			
-		}
-		introduce(){
-			return super.introduce()+' I am a Student. I am at Class '+this.klass+'.';
-		}
-		
-	}
-	var a = new a('Tom',21);
-	a.introduce();
-	var b= new ('Tom',21,2);
-	b.introduce();
-};
+module.exports = function main(a,b) {
+    return a%b;
+}
